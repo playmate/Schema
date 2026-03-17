c35bf24c9b61e3085a466bfd2cd5330397c37b5a
playmate <johan018@gmail.com>
2026-03-17 14:31:32 +0100

    Update schema_app.py

--- schema_app.py
+++ schema_app.py
@@ -254,9 +254,10 @@ def skapa_schema():
 # --- GENERERA SCHEMA KNAPP OCH VISA TIDER CHECKBOX ---
 col_gen = st.columns([1,1])
 with col_gen[0]:
-    generate = st.button("Generera schema")
-with col_gen[0]:
     visa_tider = st.checkbox("Visa tider i schemat", value=False)
+with col_gen[0]:
+    generate = st.button("Generera schema")
+
 
 # --- GENERERA SCHEMA LOGIK ---
 if generate:
